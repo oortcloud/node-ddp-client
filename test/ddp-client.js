d5a3baf624702f1f1559cf49b75728d35225e485
Thomas Sarlandie <thomas@sarlandie.net>
2013-05-09 20:08:38 +0000

    Adding a basic test infrastructure and a few tests (closes #12)

---
+++ test/ddp-client.js
@@ -0,0 +1,68 @@
+var assert = require('assert'),
+    sinon = require('sinon'),
+    rewire = require('rewire'),
+    events = require('events');
+
+var DDPClient = rewire("../lib/ddp-client");
+
+var wsConstructor, wsMock;
+
+function prepareMocks() {
+  wsMock = new events.EventEmitter();
+
+  wsConstructor = sinon.stub();
+  wsConstructor.returns(wsMock);
+  DDPClient.__set__('WebSocket', wsConstructor);
+}
+
+
+describe("Connect to remote server", function() {
+  beforeEach(function() {
+    prepareMocks();
+  });
+
+  it('should connect to the provided host', function() {
+    new DDPClient({'host': 'myserver.com'}).connect();
+    assert.deepEqual(wsConstructor.args, [['ws://myserver.com:3000/websocket']]);
+  });
+  it('should connect to the provided host and port', function() {
+    new DDPClient({'host': 'myserver.com', 'port': 42}).connect();
+    assert.deepEqual(wsConstructor.args, [['ws://myserver.com:42/websocket']]);
+  });
+  it('should use ssl if the port is 443', function() {
+    new DDPClient({'host': 'myserver.com', 'port': 443}).connect();
+    assert.deepEqual(wsConstructor.args, [['wss://myserver.com:443/websocket']]);
+  });
+});
+
+describe('Automatic reconnection', function() {
+  beforeEach(function() {
+    prepareMocks();
+  });
+
+
+  /* I am not sure why this test does not work. Something I am not doing well
+     with clock.tick() - I am leaving it here, hoping to understand why sometime soon...
+
+  it('should reconnect when the connection fails', function() {
+    var clock = sinon.useFakeTimers();
+
+    var ddpclient = new DDPClient();
+
+    ddpclient.connect();
+    wsMock.emit('error', {});
+
+    // At this point, the constructor should have been called only once.
+    assert(wsConstructor.calledOnce);
+
+    // Wait 501 ms
+    clock.tick(501);
+
+    // Now the constructor should have been called twice
+    console.log("Constructor called: %s time(s)", wsConstructor.callCount);
+    assert(wsConstructor.calledTwice);
+
+    clock.restore();
+  });
+  */
+});
